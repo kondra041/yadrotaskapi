bfedcec01c3b24f520156810c828e46cb9026c1c
kondryukov <kondra041@yandex.ru>
2026-05-26 07:39:50 +0300

    исправления docker-compose 2

--- frontend/src/shared/api/apiClient.js
+++ frontend/src/shared/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_SERVER
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 apiClient.interceptors.response.use(
